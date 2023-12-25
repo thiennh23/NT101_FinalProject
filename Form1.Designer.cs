55b01bed3d27b4437eab0a8cbe59a737b5fdb770
Nguyễn Hữu Thiện <135424236+thiennh23@users.noreply.github.com>
2023-12-25 13:17:06 +0700

    OK

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -46,9 +46,10 @@
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.Color.Coral;
-            this.button1.Location = new System.Drawing.Point(157, 288);
+            this.button1.Location = new System.Drawing.Point(209, 354);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(189, 106);
+            this.button1.Size = new System.Drawing.Size(252, 130);
             this.button1.TabIndex = 0;
             this.button1.Text = "PLAYFAIR";
             this.button1.UseVisualStyleBackColor = true;
@@ -58,9 +59,10 @@
             // 
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button2.ForeColor = System.Drawing.Color.Coral;
-            this.button2.Location = new System.Drawing.Point(375, 288);
+            this.button2.Location = new System.Drawing.Point(500, 354);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(189, 106);
+            this.button2.Size = new System.Drawing.Size(252, 130);
             this.button2.TabIndex = 1;
             this.button2.Text = "RSA";
             this.button2.UseVisualStyleBackColor = true;
@@ -70,9 +72,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(238, 23);
+            this.label1.Location = new System.Drawing.Point(317, 28);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(251, 31);
+            this.label1.Size = new System.Drawing.Size(311, 39);
             this.label1.TabIndex = 2;
             this.label1.Text = "ĐỒ ÁN MÔN HỌC";
             // 
@@ -80,9 +83,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(92, 63);
+            this.label2.Location = new System.Drawing.Point(123, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(529, 46);
+            this.label2.Size = new System.Drawing.Size(660, 58);
             this.label2.TabIndex = 3;
             this.label2.Text = "AN TOÀN MẠNG MÁY TÍNH";
             // 
@@ -90,19 +94,21 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(239, 118);
+            this.label3.Location = new System.Drawing.Point(253, 145);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(245, 25);
+            this.label3.Size = new System.Drawing.Size(461, 31);
             this.label3.TabIndex = 4;
-            this.label3.Text = "LỚP: NT101.O11.MMCL";
+            this.label3.Text = "NHÓM: 12 ; LỚP: NT101.O11.MMCL";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(275, 155);
+            this.label4.Location = new System.Drawing.Point(367, 191);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(181, 22);
+            this.label4.Size = new System.Drawing.Size(229, 26);
             this.label4.TabIndex = 5;
             this.label4.Text = "CÁC THÀNH VIÊN:";
             // 
@@ -110,9 +116,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(168, 252);
+            this.label5.Location = new System.Drawing.Point(224, 310);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(193, 21);
+            this.label5.Size = new System.Drawing.Size(246, 27);
             this.label5.TabIndex = 6;
             this.label5.Text = "NGUYỄN HỮU THIỆN";
             // 
@@ -120,9 +127,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(168, 219);
+            this.label6.Location = new System.Drawing.Point(224, 270);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(178, 21);
+            this.label6.Size = new System.Drawing.Size(227, 27);
             this.label6.TabIndex = 7;
             this.label6.Text = "LÊ THỊ NGỌC DIỄM";
             // 
@@ -130,9 +138,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(168, 188);
+            this.label7.Location = new System.Drawing.Point(224, 231);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(133, 21);
+            this.label7.Size = new System.Drawing.Size(167, 27);
             this.label7.TabIndex = 8;
             this.label7.Text = "VÕ TÍN THIỆN";
             // 
@@ -140,9 +149,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(415, 188);
+            this.label8.Location = new System.Drawing.Point(553, 231);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(141, 21);
+            this.label8.Size = new System.Drawing.Size(179, 27);
             this.label8.TabIndex = 11;
             this.label8.Text = "MSSV: 21521467";
             // 
@@ -150,9 +160,10 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(415, 219);
+            this.label9.Location = new System.Drawing.Point(553, 270);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(141, 21);
+            this.label9.Size = new System.Drawing.Size(179, 27);
             this.label9.TabIndex = 10;
             this.label9.Text = "MSSV: 21520186";
             // 
@@ -160,17 +171,18 @@
             // 
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(415, 252);
+            this.label10.Location = new System.Drawing.Point(553, 310);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(141, 21);
+            this.label10.Size = new System.Drawing.Size(179, 27);
             this.label10.TabIndex = 9;
             this.label10.Text = "MSSV: 21522626";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(725, 431);
+            this.ClientSize = new System.Drawing.Size(967, 530);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label10);
@@ -183,8 +195,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
-            this.Text = "DO AN AN TOAN MANG";
+            this.Text = "ĐỒ ÁN NHÓM 12 - MÔN AN TOÀN MẠNG MÁY TÍNH";
             this.ResumeLayout(false);
             this.PerformLayout();
 
